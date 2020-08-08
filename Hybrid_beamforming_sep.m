d5dce1e6abd3595f27fe360a1b0e7ce015059d67
scu008 <scu008@nate.com>
2020-08-08 15:44:47 +0900

    Addition of hybrid method

---
+++ Hybrid_beamforming_sep.m
@@ -0,0 +1,145 @@
+% Sepideh algorithm test (fully-connected)
+
+% 전송 데이터 스펙 설정
+model = SCM();
+model.n_mray = scatter;
+model.n_path = path;
+cp_len = fft_len / 4;
+data_len = fft_len * mod_type;
+
+% 송신단 및 수신단에서의 총 데이터 스트림 설정
+N_s = rx_node;
+N_d = rx_node;
+N_rf = num_rf;
+
+% 송수신 각 배열 안테나의 안테나 수
+model.ant(rx_ant, tx_ant);
+N_tx = model.Ntx;
+N_rx = model.Nrx;
+
+% ber 결과 출력 벡터들
+result_ber = zeros(1, length(snr) );
+result_cap = zeros(1, length(snr) );
+
+% 채널 관련 파라미터 초기화
+H = zeros(path, fft_len+cp_len, N_rx * N_d, N_tx);
+t_He = zeros(path, N_d, N_rf);
+He = zeros(fft_len, N_d, N_rf);
+
+
+for i = 1:iter
+
+%     tic
+    for j = 1:length(snr)
+        
+        %============= 채널 계산 ============================================
+        
+        % 채널 계수 생성
+        for d = 1:N_d
+            [temp, rx_angle] = model.FD_channel(fft_len + cp_len);
+            H(:,:,1+(d-1)*N_rx:d*N_rx,:) = temp;
+        end
+        
+        % 시간 채널 주파수 변환
+        tmp_H(:,:,:) = H(:,1,:,:);
+        H_f = fft(tmp_H, fft_len, 1);        
+        
+        % Beamforming 계수 계산 (송신, 수신 계수)
+        Wt = sep_precoding(H_f, N_rf);
+        
+        %============== 신호 송신 ===========================================
+        
+        % 전송 심볼 생성
+        bit = randi([0 1], N_s, data_len);
+        Dsym = base_mod(bit, mod_type);
+        
+        % precoding 수행
+        for k = 1:fft_len
+            tmp_H_f(:,:) = H_f(k,:,:);
+            He(k,:,:) = tmp_H_f * Wt;
+        end
+        [Dsym, ~, Wd] = ZF_precoding(Dsym, He);
+        
+        % 정규화 상수 계산
+        factor = zeros(1,fft_len);
+        for k = 1:fft_len
+            t_Wd(:,:) = Wd(k,:,:);
+            temp = Wt * t_Wd;
+            factor(k) = sqrt( trace( temp * temp' ) );
+        end
+        
+        % OFDM 심볼 생성
+        Dsym = Dsym ./ factor;
+        Isym = ifft(Dsym, fft_len, 2) * sqrt(fft_len);
+        tx_ofdm = [ Isym(:, fft_len - cp_len + 1 : end) Isym ];
+        
+        
+        % 송신 빔계수 적용
+        tx_ofdm = Wt * tx_ofdm;   
+        
+        
+        %============== 신호 수신 ===========================================
+        % ========================= BER 계산 ==============================
+        % 채널 통과
+        [rx_ofdm, No] = awgn_noise( model.FD_fading( tx_ofdm, H ), snr(j) );
+        
+        % 수신 빔포밍 계수 적용
+        rx_ofdm = rx_ofdm;
+        
+        % OFDM 복조
+        rx_Isym = rx_ofdm(:, cp_len + 1 : fft_len + cp_len);
+        rx_Dsym = fft(rx_Isym, fft_len, 2) / sqrt(fft_len);
+        rx_Dsym = rx_Dsym .* factor;
+        
+        % base demodulation
+        rx_bit = base_demod(rx_Dsym, mod_type);
+        result_ber(j) = result_ber(j) + sum( sum( bit ~= rx_bit, 2 ), 1 ) / (data_len * N_s);
+        
+        
+        % ======================== Capacity 계산 ==========================
+        
+        % Capacity 계산
+        rx_ofdm = model.FD_fading( tx_ofdm, H );
+        rx_Isym = rx_ofdm(:, cp_len + 1 : fft_len + cp_len);
+        rx_Dsym = fft(rx_Isym, fft_len, 2) / sqrt(fft_len);
+        result_cap(j) = result_cap(j) + mean( sum( log2( 1 + abs(rx_Dsym).^2 / (No * rx_ant) ) ) );
+        
+        
+    end
+%     toc
+    
+end
+
+
+% 결과 평균화
+result_ber = result_ber / iter;
+result_cap = result_cap / iter;
+
+if mode == 1
+    % Capacity 결과 출력
+    plot(snr, result_cap, plot_format);
+    title('Sum Rate Performance')
+    legend('Sum Capacity')
+    ylabel('Average Spectral Efficiency (bps/Hz)')
+    xlabel('SNR (dB)')
+    grid on
+    
+elseif mode == 2
+    % BER 결과 출력
+    semilogy(snr, result_ber, plot_format);
+    title('BER Performance')
+    legend('Detection result')
+    ylabel('BER')
+    xlabel('SNR (dB)')
+    grid on
+    axis([snr(1) max(snr) 10^-5 1])
+    
+elseif mode == 3
+    % Repeatition 결과 출력
+    plot(snr, result_rep, plot_format);
+    title('Repeatition Performance')
+    ylabel('Repeatition Number')
+    xlabel('SNR (dB)')
+    grid on
+
+end
--- Main_simulation.m
+++ Main_simulation.m
@@ -9,12 +9,12 @@ fft_len = 64;               % OFDM 부반송파의 수
 mod_type = 2;               % 변조 차수 ex) 1 - BPSK, 2 - QPSK, 4 - 16QAM, 6 - 64QAM, 8 - 256QAM
 rx_node = 4;                % 수신기의 수 (수신기의 안테나는 1개)
 tx_ant = 16;                % 기지국의 안테나 수
+snr = -20:5:10;               % 전송 채널 SNR 범위
+path = 7;
+scatter = 10;
+iter = 300;                 % 전송 반복 횟수
 
 % 설정 파라미터 기반의 프로그램 실행
-scatter = 1;
-path = 1;
-snr = -20:5:10;             % 전송 채널 SNR 범위
-iter = 100;                 % 전송 반복 횟수
 plot_format = 's-b';
 FC_MIMO_testbed             % 실행 프로그램
 hold on
@@ -30,13 +30,36 @@ fft_len = 64;               % OFDM 부반송파의 수
 mod_type = 2;               % 변조 차수 ex) 1 - BPSK, 2 - QPSK, 4 - 16QAM, 6 - 64QAM, 8 - 256QAM
 rx_node = 4;                % 수신기의 수 (수신기의 안테나는 1개)
 tx_ant = 16;                % 기지국의 안테나 수
-rx_ant = 1;                 % 수신기의 개별 안테나 수
+snr = -20:5:10;               % 전송 채널 SNR 범위
+path = 7;
+scatter = 10;
+iter = 300;                 % 전송 반복 횟수
 
 % 설정 파라미터 기반의 프로그램 실행
-path = 1;
-scatter = 1;
-snr = -20:5:10;             % 전송 채널 SNR 범위
-iter = 300;                 % 전송 반복 횟수
+rx_ant = 1;                 % 수신기의 개별 안테나 수
 plot_format = 'r-*';
 Hybrid_beamforming          % 실행 프로그램
+hold on
+
+%% Sepideh beamforming
+clc, clear
+
+% 1: Capacity, 2: BER
+mode = 1;
+
+% 전송 파라미터 설정
+fft_len = 64;               % OFDM 부반송파의 수
+mod_type = 2;               % 변조 차수 ex) 1 - BPSK, 2 - QPSK, 4 - 16QAM, 6 - 64QAM, 8 - 256QAM
+rx_node = 4;                % 수신기의 수 (수신기의 안테나는 1개)
+tx_ant = 16;                % 기지국의 안테나 수
+snr = -20:5:10;               % 전송 채널 SNR 범위
+path = 7;
+scatter = 10;
+iter = 10;                 % 전송 반복 횟수
+
+% 설정 파라미터 기반의 프로그램 실행
+num_rf = 8;                 % 기지국의 RF chain의 수 *(필수 조건: rx_node <= num_rf <= tx_ant)*
+rx_ant = 1;                 % 수신기의 개별 안테나 수
+plot_format = 'm-*';
+Hybrid_beamforming_sep      % 실행 프로그램
 hold on
